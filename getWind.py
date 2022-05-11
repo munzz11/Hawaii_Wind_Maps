091942dfd1ae51fe66dfd863b9fc30c785110d96
munzz11 <munoza@wit.edu>
2022-05-11 13:50:32 -0400

    initial commit

---
+++ getWind.py
@@ -0,0 +1,58 @@
+import wget
+import pickle
+import time
+import math
+import os
+
+def roundup(x):                             # For some reason the api requires a requested timestamp ending with 00
+    return int((x / 100.0)) * 100
+
+params_file = 'parameters.pickled'
+parameters = pickle.load(open(params_file, 'rb')) 
+
+start_time = parameters['start_time']
+interval = parameters['interval']
+num_days = parameters['num_days']
+ymin = parameters['ymin']
+ymax = parameters['ymax']
+xmin = parameters['xmin']
+xmax = parameters['xmax']
+
+if start_time == '0':
+    utc_timestamp = int(time.time()//3600*3600)  
+    request_timestamp = roundup(utc_timestamp)
+hours_requested = int(num_days) * 24
+url = ''    
+while hours_requested > 0 :
+    request_timestamp = request_timestamp + 3600
+    url = ('https://www.pacioos.hawaii.edu/cgi-bin/get_screenshot.py?url=http%3A//www.pacioos.hawaii.edu/voyager/index.html%3Fb%3D'+ 
+        ymin + 
+        '%2C' + 
+        xmin + 
+        '%2C' + 
+        ymax + 
+        '%2C' + 
+        xmax + 
+        '%26o%3Dwfore%3A2%3Amph%3Ad3s2t' + 
+        str(request_timestamp) + 
+        '%26output%3Dprint&format=png&width=1920&height=1080&wait_for_dom_id=map&crop_dom_id=map&v=' + 
+        str(int(time.time()*1000)))
+    
+    readable_request = time.ctime((request_timestamp))
+    print("Requesting rendered wind map for " + readable_request)
+    
+    path = os.path.join(os.getcwd(), readable_request.replace(' ','_')[:10])
+    filename = readable_request.replace(' ','_')[4:] + '.png'
+    outfile = os.path.join(path,filename) 
+
+    if not os.path.exists(path):
+        os.mkdir(path)
+    print('Saving to: ' + outfile)
+
+    wget.download(url,outfile)
+    #print('wget --directory-prefix=' + path + ' -O ' + filename + ' ' + url + ', verbose = False')
+
+    hours_requested = hours_requested - 1
+    #time.sleep(2)
+    
+
---
+++ params.py
@@ -0,0 +1,23 @@
+
+############################### Parameters for getWind.py ###############################
+
+parameters = {
+    'start_time': '0',                  # Not working yet leave as 0, eventually will be able to use YYYY-DD-MMThh:mm in UTC or 0 for current time
+    'num_days': '5',                    # Max 6 days of forcast
+    'interval': '1',                    # Interval in hours e.g, 1 = every hour, 2 = every other hour
+    'ymin' : '20.501959',               # The following define the BBox for the map to render, its supposedly based on EPSG:4326 but I'm not sure thats accurate   
+    'ymax' : '20.913021',
+    'xmin' : '-157.155304',
+    'xmax' : '-156.297684'
+    }
+
+#########################################################################################
+
+
+
+import pickle
+
+file_name = 'parameters.pickled'
+
+with open(file_name, 'wb') as out_file:
+  pickle.dump(parameters, out_file)
